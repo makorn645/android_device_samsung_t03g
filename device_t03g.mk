afd239fc3d1096d6005fcf7ee26715730a0cb6db
Makornthawat Emery <makornthawateugeneemery@gmail.com>
2017-04-16 13:21:36 +0700

    Update

--- device_t03g.mk
+++ device_t03g.mk
@@ -1,10 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
-$(call inherit-product-if-exists, vendor/samsung/t03g/t0lteatt-vendor.mk)
-
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 LOCAL_PATH := $(LOCAL_PATH)
