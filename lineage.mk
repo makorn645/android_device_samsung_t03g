dca7823e72a24c22f64fff51ba5bb020dab366d9
makorn645 <makornthawateugeneemery@gmail.com>
2017-08-02 18:23:16 +0200

    t03g: twrp cleanup
    Change-Id: Id8120f7ec80df93e4b698bf181b8c5293d218834

--- lineage.mk
+++ lineage.mk
@@ -34,9 +34,6 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit some common stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/n7100/full_n7100.mk)
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n7100
 PRODUCT_NAME := lineage_n7100
--- n7100.mk
+++ n7100.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Include common makefile
-$(call inherit-product, device/samsung/smdk4412-common/common.mk)
-
 LOCAL_PATH := device/samsung/n7100
 
 # Overlay
@@ -31,14 +28,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.smdk4x12:root/fstab.smdk4x12 \
     $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc
 
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/sound/t03g
-
-# Camera
-PRODUCT_PACKAGES += \
-    camera.smdk4x12
-
 # f2fs
 PRODUCT_PACKAGES += \
     fibmap.f2fs \
@@ -47,79 +36,12 @@ PRODUCT_PACKAGES += \
 
 # Busybox
 PRODUCT_PACKAGES += \
-    busybox
-
-# Gps
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.xml:system/etc/gps.xml
-
-# Product specific Packages
-PRODUCT_PACKAGES += \
-    DeviceSettings \
-    SamsungServiceMode
-
-# RIL & GPS fix
-PRODUCT_PACKAGES += \
-    libsecril-shim \
-    libdmitry \
-    libshim_gpsd
-
-# Additional apps
-PRODUCT_PACKAGES += \
-    OpenDelta
+     busybox
 
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.smdk4x12
 
-# NFC
-PRODUCT_PACKAGES += \
-    nfc.exynos4 \
-    libnfc \
-    libnfc_jni \
-    Nfc \
-    Tag
-
-PRODUCT_COPY_FILES += \
-    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
-
-# NFCEE access control
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access.xml
-else
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access_debug.xml
-endif
-
-PRODUCT_COPY_FILES += \
-    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
-
-PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
-    Stk
-
-# Samsung symbols
-PRODUCT_PACKAGES += \
-    libsamsung_symbols
-
-# RIL
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=SamsungExynos4RIL \
-    mobiledata.interfaces=pdp0,gprs,ppp0,rmnet0,rmnet1 \
-    ro.telephony.call_ring.multiple=false \
-    ro.telephony.call_ring.delay=3000
-
-# These are the hardware-specific features
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
-
-# Barometer
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml
-
 # Allow tethering without provisioning app
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
-
-$(call inherit-product-if-exists, vendor/samsung/n7100/n7100-vendor.mk)
